be89a197fdb63994d3442e49594fbfa6f3be2096
Павел <paul-36@mail.ru>
2021-03-09 23:14:18 +0300

    working on models

--- app_01_dishes/models.py
+++ app_01_dishes/models.py
@@ -1,18 +1,23 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-# from . import choices
 
 
 User = get_user_model()
 
 
 class Ingredient(models.Model):
-    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     measure = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -21,16 +26,47 @@ class Recipe(models.Model):
         related_name="reciept",
         verbose_name='Автор'
         )
-    title = models.CharField(max_length=50, verbose_name='Название')
+    name = models.CharField(
+        max_length=50,
+        verbose_name='Название',
+        db_index=True
+        )
     image = models.ImageField(
         upload_to='app_01_dishes/',
         verbose_name='Картинка'
         )
-    description = models.TextField(verbose_name='Текстовое описание')
+    description = models.TextField(
+        verbose_name='Текстовое описание'
+        )
     ingredients = models.ManyToManyField(
         Ingredient,
-        verbose_name='Ингридиенты'
+        through='Volume',
+        verbose_name='Ингридиенты',
+        db_index=True
         )
-    tag = models.ManyToManyField(Tag, verbose_name='Тэг')
-    time = models.IntegerField()
+    tag = models.ManyToManyField(
+        Tag,
+        verbose_name='Тэг',
+        db_index=True
+        )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True, db_index=True
+        )
+    cook_time = models.IntegerField()
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Volume(models.Model):
+    id_recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        )
+    id_ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+        )
+    volume = models.IntegerField()
--- foodgram_project/urls.py
+++ foodgram_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_01_dishes.urls')),
+    # path('', include('app_01_dishes.urls')),
 ]
