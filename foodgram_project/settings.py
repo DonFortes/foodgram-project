a171ac0588b16ed2365427e83c9a8ec34c77c686
Павел <paul-36@mail.ru>
2021-03-13 16:48:26 +0300

    favorite.html

--- foodgram_project/settings.py
+++ foodgram_project/settings.py
@@ -130,4 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = 'auth/signup_done/'
