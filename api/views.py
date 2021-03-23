7b3b5276e16d4c26d1df063244787bf13cace6ea
Павел <paul-36@mail.ru>
2021-03-23 17:35:42 +0300

    workin on api views

--- api/views.py
+++ api/views.py
@@ -9,15 +9,10 @@ from django.views.decorators.http import require_http_methods
 @login_required
 @require_http_methods("POST")
 def purchases(request):
-    recipe_id = json.loads(request.body).get('id')
-    if recipe_id is None:
-        return JsonResponse({"success": False})
-    recipe_id = int(recipe_id)
+    recipe_id = int(json.loads(request.body).get('id'))
     recipe = get_object_or_404(Recipe, pk=recipe_id)
-
     if request.user not in recipe.basket.all():
         recipe.basket.add(request.user)
-        recipe.save()
         return JsonResponse({'success': True})
     else:
         return JsonResponse({'success': False})
@@ -33,34 +28,25 @@ def purchases_delete(request, recipe_id):
 
 
 @login_required
+@require_http_methods("POST")
 def subscriptions(request, id=None):
-    if request.method == 'POST':
-        user_id = json.loads(request.body).get("id")
-        if user_id is None:
-            return JsonResponse({"success": False})
-        user_id = int(user_id)
-        if User.objects.filter(pk=user_id).exists():
-            following = User.objects.get(pk=user_id)
-            if following not in request.user.following.all():
-                request.user.following.add(following)
-        return JsonResponse({"success": True})
+    author_id = int(json.loads(request.body).get("id"))
+    author = get_object_or_404(pk=author_id)
+    user = request.user
+    if author != user:
+        Follow.objects.get_or_create(
+            user=user,
+            author=author
+        )
+        return JsonResponse({'success': True})
     return JsonResponse({"success": False})
 
 
-        # if user is None:
-        #     Follow.objects.get_or_create(
-        #         user=user,
-        #         author=author,
-        #     )
-        #     return JsonResponse({"success": False})
 
-    # elif request.method == 'DELETE':
-    #     author = get_object_or_404(User, pk=id)
-    #     user = request.user
-    #     Follow.objects.filter(user=user, author=author).delete()
-
-    # return JsonResponse({'success': 'true'})
-
+@login_required
+def subscriptions_delete(request, id=None):
+    
+    return JsonResponse({'success': 'true'})
 
 @login_required
 def favorites(request):
@@ -69,6 +55,5 @@ def favorites(request):
 
 
 def single_recipe(request, slug):
-    recipe = get_object_or_404(Recipe, slug)
 
-    pass
+    return JsonResponse({'success': 'true'})
