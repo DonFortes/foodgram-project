102d4541325571f966e24345f6d45f727e50439f
Павел <paul-36@mail.ru>
2021-03-24 00:38:14 +0300

    ordering urls, new recipe works

--- dishes/urls.py
+++ dishes/urls.py
@@ -3,9 +3,9 @@ from dishes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new_recipe/', views.new_recipe, name='new_recipe'),
     path('<str:username>/', views.profile, name="profile"),
     path('following/', views.following, name='following'),
-    path('new_recipe/', views.new_recipe, name='new_recipe'),
     path('recipe/<slug:slug>/', views.single_recipe, name='single_recipe'),
     path('recipe/<slug:slug>/edit/', views.edit_recipe, name='edit_recipe'),
     path('recipe/<slug:slug>/delete/', views.delete_recipe,
